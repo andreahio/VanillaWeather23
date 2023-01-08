94eb45237e26a43a277715f53e28c6740952cf38
AndreaHiott <andreahiottcontact@gmail.com>
2023-01-08 15:13:18 +0100

    added real link to humidity and description

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,12 @@
 function displayTemperature(response) {
 let temperatureElement= document.querySelector("#temperature");
+let cityElement= document.querySelector("#city");
+let descriptionElement= document.querySelector("#description");
+let humidityElement= document.querySelector("#humidity");
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey= "9e30b534bf5fe13f955b2b8fa22fe5a6";

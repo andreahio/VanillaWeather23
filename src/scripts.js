9ebc977649719d290ff107c5631ff82f9cbd902b
AndreaHiott <andreahiottcontact@gmail.com>
2023-01-08 15:20:33 +0100

    added real link to wind data

--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,12 @@ let temperatureElement= document.querySelector("#temperature");
 let cityElement= document.querySelector("#city");
 let descriptionElement= document.querySelector("#description");
 let humidityElement= document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey= "9e30b534bf5fe13f955b2b8fa22fe5a6";

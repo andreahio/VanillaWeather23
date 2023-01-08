93069d17f6471547f91f3126e4141e3871e8bf65
AndreaHiott <andreahiottcontact@gmail.com>
2023-01-08 16:08:11 +0100

    updated link to realtime weather icon

--- src/scripts.js
+++ src/scripts.js
@@ -21,12 +21,15 @@ let descriptionElement= document.querySelector("#description");
 let humidityElement= document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);  
 }
 
 let apiKey = "9e30b534bf5fe13f955b2b8fa22fe5a6";
